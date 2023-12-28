643165dcd106beb3e740611ee3f497c4d9e1d0c3
Samy Hamidou <samyhamidou@gmail.com>
2023-12-28 11:40:08 +0100

    Update wirguard_exporter.go

--- wirguard_exporter.go
+++ wirguard_exporter.go
@@ -1,16 +1,3 @@
-# This code sets up a Node Exporter for Prometheus in Go to check WireGuard logs.
-
-# The Node Exporter is a Prometheus exporter that collects metrics from the host machine.
-# It is written in Go and provides various system-level metrics such as CPU usage, memory usage, disk usage, and network statistics.
-
-# In this code, we are configuring the Node Exporter to specifically monitor WireGuard logs.
-# WireGuard is a modern VPN protocol that provides secure and fast communication between devices.
-
-# To use this code, make sure you have Go installed on your machine and the necessary dependencies for building and running the Node Exporter.
-
-# Once the Node Exporter is up and running, you can use Prometheus to scrape the metrics exposed by the Node Exporter and visualize them in a dashboard.
-
-# For more information on how to configure and use the Node Exporter, refer to the official documentation: https://github.com/prometheus/node_exporter
 package main
 
 import (
